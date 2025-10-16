6f1aef077099445f8b445ceccdc27ed6ba7a0127
Sergei Miliaev <sergey.milyaev@gmail.com>
2025-10-16 12:29:26 +0200

    Add CUDA installation notes to setup script
    Added comments about CUDA version requirements.

--- tensorrt-llm_colab_setup.sh
+++ tensorrt-llm_colab_setup.sh
@@ -1,2 +1,4 @@
 #!bin/bash
+# TensorRT-LLM may require installation of a newer CUDA version, for example:
+# sudo apt install cuda-12-8
 pip3 install --upgrade pip setuptools && pip3 install tensorrt_llm
