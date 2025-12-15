011c81e1748511059870bfd05414d5f63ea35911
SergeyMilyaev <sergey.milyaev@.com>
2025-12-15 21:22:07 +0000

    TensorRT-LLM installation script updated

--- tensorrt-llm_colab_setup.sh
+++ tensorrt-llm_colab_setup.sh
@@ -1,5 +1,12 @@
-#!bin/bash
-# TensorRT-LLM may require installation of a newer CUDA version, for example:
-# sudo apt-get update && sudo apt install cuda-12-8
-pip3 install --upgrade pip setuptools && pip3 install tensorrt_llm
-#pip3 install --upgrade omegaconf
+#!/bin/sh
+apt-get update && apt-get -y install cuda-12-8 git git-lfs
+git lfs install
+
+TRTLLM_VER="1.1.0rc5"
+git clone -b v$TRTLLM_VER https://github.com/NVIDIA/TensorRT-LLM.git
+cd TensorRT-LLM
+git submodule update --init --recursive
+git lfs pull
+
+TRTLLM_USE_PRECOMPILED=$TRTLLM_VER pip -v wheel . --no-deps --wheel-dir ./build
+pip install ./build/tensorrt_llm*.whl
