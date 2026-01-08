5908583822d8dfcdb30cac70a25671cb494dde8b
SergeyMilyaev <sergey.milyaev@.com>
2026-01-08 22:00:22 +0000

    TensorRT-LLM installation changed to pre-built wheel

--- tensorrt-llm_colab_setup.sh
+++ tensorrt-llm_colab_setup.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-apt-get update && apt-get -y install cuda-12-8 git git-lfs
-git lfs install
+apt-get update && apt-get -y install cuda-12-8
 
-TRTLLM_VER="1.1.0rc5"
-git clone -b v$TRTLLM_VER https://github.com/NVIDIA/TensorRT-LLM.git
-cd TensorRT-LLM
-git submodule update --init --recursive
-git lfs pull
-
-TRTLLM_USE_PRECOMPILED=$TRTLLM_VER pip -v wheel . --no-deps --wheel-dir ./build
-pip install ./build/tensorrt_llm*.whl
+pip3 install -r requirements.txt
+git clone -b v1.1.0rc5 https://github.com/NVIDIA/TensorRT-LLM.git
