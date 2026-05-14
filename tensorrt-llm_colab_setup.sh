d484e5fbcf3998b0aa6d9f422d345eb7f971bfbc
SergeyMilyaev <sergey.milyaev@gmail.com>
2026-05-14 13:10:34 +0000

    Colab TensorRT-LLM installation script updated

--- tensorrt-llm_colab_setup.sh
+++ tensorrt-llm_colab_setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-apt-get update && apt-get -y install cuda-12-8
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+apt-get update
+apt-get install -y cuda-toolkit-13-0
 
-pip3 install -r requirements.txt
-git clone -b v1.1.0rc5 https://github.com/NVIDIA/TensorRT-LLM.git
+export PATH=/usr/local/cuda-13.0/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/local/cuda-13.0/lib64:${LD_LIBRARY_PATH}
+
+pip uninstall -y torch torchvision torchaudio
+pip3 install torch==2.10.0 torchvision --index-url https://download.pytorch.org/whl/cu130
+
+apt-get -y install libopenmpi-dev
+apt-get -y install libzmq3-dev
+
+pip3 install --ignore-installed pip setuptools wheel && pip3 install tensorrt_llm==1.3.0rc14
